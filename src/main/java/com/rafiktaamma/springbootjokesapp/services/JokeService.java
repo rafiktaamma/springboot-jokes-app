a9a455d2420251cb188ecd8577af405881b391fd
rafik <b.taamma@esi-sba.dz>
2021-11-17 10:34:00 +0100

    Adding a service layer. closes #2

---
+++ src/main/java/com/rafiktaamma/springbootjokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.rafiktaamma.springbootjokesapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/com/rafiktaamma/springbootjokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.rafiktaamma.springbootjokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(){
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
